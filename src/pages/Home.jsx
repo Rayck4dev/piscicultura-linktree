3e72dee6b6646b81de7a16ce34e88d5345bf4395
Raycka Castro <messacastrottoo@gmail.com>
2026-02-06 16:07:17 -0400

    style: corrigindo altura do footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -87,7 +87,7 @@ export default function Home() {
         </a>
       </div>
 
-      <footer className="relative z-10 text-sm text-gray-200 font-sans text-center py-6">
+      <footer className="relative z-10 text-sm text-gray-200 font-sans text-center py-6 mb-6">
         © 2026 Piscicultura Magrão Alevinos
         <div className="max-w-xl mx-auto flex flex-col sm:flex-row items-center justify-center gap-2 text-center mt-2">
           <a
